fd5ea76d2d703d71510494ecd57017099856e7ea
Mariuskvaal <mariuskvaal1@gmail.com>
2022-06-02 13:22:15 +0200

    doing the tasked

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// Question 1
+//
+var requiredName = "Gertrude";
+var suppliedName = "GERTRUDE";
+
+var lower = requiredName.toUpperCase();
+
+console.log(lower);
+
+
+// Question 2
+//
+
+var userName = "myusername";
+var minimumCharacter = 4;
+var maximunCharacter = 10;
+
+
+if (userName.length >= minimumCharacter && userName.length <= maximunCharacter ) {
+    console.log ("Acceptable username");
+}
+else {
+
+        if (userName.length < minimumCharacter) {
+        console.log ("Too few characters");
+        }
+
+        if (userName.length > maximunCharacter) {
+        console.log ("Too many characters");
+        }
+    }
+
+// Question 3
+//
+
+var invoicePaid = true;
+var productDispatched = false;
+var customerHasSigned = false;
+
+if ((invoicePaid === true) && (productDispatched) === true && (customerHasSigned === true)) {
+    console.log("Order complete");
+}
+else {
+    if (invoicePaid !== true) {
+        console.log("Invoice must be paid")
+    }
+
+    if (productDispatched !== true) {
+            console.log("Product is not dispatched")
+        }
+
+    if (customerHasSigned !== true) {
+            console.log("Customer need to sign")
+        }
+    
+}
